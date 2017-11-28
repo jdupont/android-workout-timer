d1ebe504a1e7904d5b26c6a567ae0e0c1ad3fbe8
Jules <jules.dupont@outlook.com>
2017-11-28 17:27:03 -0500

    Some kind of PMD analysis running -- now just need to integrate with the rest of the build system.

--- app/src/main/java/com/budgebars/rotelle/workouts/consumers/ExerciseEditedConsumer.java
+++ app/src/main/java/com/budgebars/rotelle/workouts/consumers/ExerciseEditedConsumer.java
@@ -1,10 +1,12 @@
 package com.budgebars.rotelle.workouts.consumers;
 
+import java.io.Serializable;
+
 /**
  * Created by Jules on 10/18/2017.
  */
 
-public interface ExerciseEditedConsumer {
+public interface ExerciseEditedConsumer extends Serializable {
   public void exerciseEdited(final EditAction action);
 
   public enum EditAction {
